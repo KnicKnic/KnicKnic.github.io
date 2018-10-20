6a9bfb0e0da80c9f16d7a1ee081be56477c35171
KnicKnic <35521284+KnicKnic@users.noreply.github.com>
2018-10-20 14:18:12 -0700

    update code to split out webtoon code

--- croppy/add_croppy.js
+++ croppy/add_croppy.js
@@ -27,9 +27,7 @@ function receiveCroppyMessage(event) {
     {
         fileNames = fileNames + " " + file.name;
     }
-    alert("Got Call back " + fileNames);
-    // event.source is popup
-    // event.data is "hi there yourself!  the secret response is: rheeeeet!"
+    // alert("Got Call back " + fileNames);
     processFilePromise['resolve'](fileObjects);
 }
 window.addEventListener('message', receiveCroppyMessage, false);
@@ -100,6 +98,7 @@ BrowseButton.prototype.getFiles = function (){
 UploadQueue.prototype._oldQueue = UploadQueue.prototype._addQueue;
 var processFilePromise = CreatePromiseEvent();
 
+//update code such that only working if doing upload queue. look at elements in self
 UploadQueue.prototype._addQueue = function(files) {
     var self = this;
     processFilePromise = CreatePromiseEvent();
@@ -113,59 +112,7 @@ UploadQueue.prototype._addQueue = function(files) {
 };
 
 
-UploadQueue.prototype._uploadFileForHTML5 = function(oProp) {
-    var oFile = oProp.file;
-    var sKey = oProp.key;
-    oFile.state = this._states.PROGRESS;
-    this._uploading++;
-    var oXHR = new XMLHttpRequest()
-      , oUpload = oXHR.upload;
-    oUpload.addEventListener("progress", $.proxy(function(e) {
-        if (!e.lengthComputable) {
-            return
-        }
-        if (!this._xhr[sKey]) {
-            return
-        }
-        oFile.loaded = e.loaded;
-        oFile.rate = e.loaded && (e.loaded / oFile.size);
-        this.trigger("uploadProgress", {
-            oFile: oFile,
-            sKey: sKey
-        })
-    }, this), false);
-    var proxyFunc = $.proxy(function() {
-        // var that = this;
-        // var func = arguments.callee;
-        if (oXHR.readyState !== 4) {
-            setTimeout(function() {
-                proxyFunc();
-            }, 0);
-            return
-        }
-        if (oXHR.status === 200) {
-            this._setDoneState(sKey, oFile, "uploadLoad", oFile.size, 1, this._states.LOAD, oXHR.responseText)
-        } else {
-            this._setDoneState(sKey, oFile, "uploadError", 0, 0, this._states.ERROR)
-        }
-    }, this);
-    oUpload.addEventListener("load", proxyFunc, false);
-    oUpload.addEventListener("error", $.proxy(function() {
-        this._setDoneState(sKey, oFile, "uploadError", 0, 0, this._states.ERROR)
-    }, this), false);
-    oUpload.addEventListener("abort", $.proxy(function() {
-        this._setDoneState(sKey, oFile, "uploadAbort", 0, 0, this._states.NONE)
-    }, this), false);
-    oXHR.open("POST", this.url());
-    oXHR.setRequestHeader("Cache-Control", "no-cache");
-    oXHR.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-    this._xhr[sKey] = oXHR;
-    this.trigger("uploadStart", {
-        oFile: oFile,
-        sKey: sKey
-    });
-    var oFormData = new FormData();
-    oFormData.append(this.option("sParamName"), oFile, oFile.name);
-    oXHR.send(oFormData);
-    return true
-}
+// import the webtoon code that was modified
+// kept in another file due to licensing
+import * as Webtoon from 'https://knicknic.github.io/croppy/webtoon.js';
+UploadQueue.prototype._uploadFileForHTML5 = Webtoon._uploadFileForHTML5
